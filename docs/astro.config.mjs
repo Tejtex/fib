2024cf4bc3ad10d2823b61a3d85754a7037fa341
Tejtex <raxeli1.mc@gmail.com>
2025-09-21 19:46:15 +0200

    a

--- docs/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
-import tailwind from "@tailwindcss/vite";
-
-export default defineConfig({
-    base: '/fib/',
-    integrations: [tailwind()],
-    output: 'static'
-});
