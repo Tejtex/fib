51ce09c35491df54635559cb59615e859930c182
Tejtex <raxeli1.mc@gmail.com>
2025-09-21 18:56:15 +0200

    dsf

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import tailwind from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+    base: '/fib/',
+    integrations: [tailwind()],
+    output: 'static'
 });
